698179f33ac73a689189123449728d53d3441173
Md Firdosh Ansari <115987354+Firdosh01@users.noreply.github.com>
2023-08-22 23:21:25 -0500

    update endpoints

--- src/services/operations/apis.js
+++ src/services/operations/apis.js
@@ -2,6 +2,6 @@ const BASE_URL = process.env.REACT_APP_BASE_URL
 
 // AUTH ENDPOINTS
 export const endpoints = {
-  SIGNUP_API: BASE_URL + "/signup",
-  LOGIN_API: BASE_URL + "/login",
+  SIGNUP_API: BASE_URL + "/auth/signup",
+  LOGIN_API: BASE_URL + "/auth/login",
 }
